44945912c41d2b1bee1df2e29fc75c3d42a14996
EngSaladKadie <zaladyusuf@gmail.com>
2024-07-22 15:29:57 +0300

    Deployment

--- src/Commponent/maninPage.js
+++ src/Commponent/maninPage.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react'; // Importing necessary modules from React
 import MealCard from './mealCard';  // Importing the MealCard component
-// import SignUpForm from '../SignUpForm'; // Importing the SignUpForm component
 import { Link } from 'react-router-dom'; // Importing Link from react-router-dom for navigation
 
 const MainPage = () => {
@@ -19,36 +18,32 @@ const MainPage = () => {
       setMSG("Please enter something"); // Setting message if search input is empty
     } else {
       try {
-        const get = await fetch(`http://www.themealdb.com/api/json/v1/1/search.php?s=${search}`);
+        const get = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${search}`);
         const jsonData = await get.json(); // Fetching data from the meal API based on search input
         setData(jsonData.meals); // Updating the data state with fetched meal data
+        setMSG(""); // Clear message after successful data fetch
       } catch (error) {
         console.error("Error fetching meal data:", error); // Handling errors
+        setMSG("Error fetching data");
       }
     }
   };
 
   return (
-    <>
-      <div>
-        <h1 className='head'>FOOD RECIPE APP</h1>
-        <div className='container'>
-          <div className='searchBar'>
-            <input type='text' placeholder='Enter dish' onChange={handleInput} />
-            <button onClick={myfun}>Search</button>
-            <Link to="/signup">
-              <button className='btn-primary px-3 py-2'>SignUp</button>
-            </Link>
-          </div>
-
-          <h4>{msg}</h4>
-
-          <div>
-            <MealCard detail={data} />
-          </div>
+    <div>
+      <h1 className='head'>FOOD RECIPE APP</h1>
+      <div className='container'>
+        <div className='searchBar'>
+          <input type='text' placeholder='Enter dish' onChange={handleInput} />
+          <button onClick={myfun}>Search</button>
+          <Link to="/signup">
+            <button className='btn-primary px-3 py-2'>SignUp</button>
+          </Link>
         </div>
+        <h4>{msg}</h4>
+        {data && <MealCard detail={data} />}
       </div>
-    </>
+    </div>
   );
 };
 
--- src/Commponent/mealCard.js
+++ src/Commponent/mealCard.js
@@ -2,17 +2,19 @@ import React from 'react'; // Importing necessary modules from React
 import { NavLink } from 'react-router-dom'; // Importing NavLink for navigation
 
 const MealCard = ({ detail }) => {
+  if (!detail) {
+    return <p>No meals found</p>; // Handling case where detail is not provided
+  }
+
   return (
     <div className='meals'>
-      {!detail ? "" : detail.map((curItem) => {
-        return (
-          <div key={curItem.idMeal} className='mealImg'>
-            <img src={curItem.strMealThumb} alt={curItem.strMeal} />
-            <p>{curItem.strMeal}</p>
-            <NavLink to={`/${curItem.idMeal}`}><button>Recipe</button></NavLink>
-          </div>
-        );
-      })}
+      {detail.map((curItem) => (
+        <div key={curItem.idMeal} className='mealImg'>
+          <img src={curItem.strMealThumb} alt={curItem.strMeal} />
+          <p>{curItem.strMeal}</p>
+          <NavLink to={`/${curItem.idMeal}`}><button>Recipe</button></NavLink>
+        </div>
+      ))}
     </div>
   );
 };
